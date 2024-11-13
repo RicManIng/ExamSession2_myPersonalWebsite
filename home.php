714948b94e82cae236b30e49d689c10fee0a8beb
Riccardo Mancinelli <riccardo.mancinelli@cherrybytes.it>
2024-11-13 06:30:26 +0100

    home page v1.0

--- home.php
+++ home.php
@@ -10,13 +10,18 @@
 <body>
     <?php
         require_once('common.php');
-        require_once('header.php');
+        //require_once('header.php');
     ?>
 
     <main>
+        <div class="home-container">
+            <h1>Web and IoT Innovation for Your Business Future</h1>
+            <h2>From web development to embedded systems, we take your business to the next level.</h2>
+            <a href="what_about.php?selected=1">See more information</a>
+        </div>
     </main>
     <?php
-        require_once('footer.php');
+        //require_once('footer.php');
     ?>
 </body>
 </html>
