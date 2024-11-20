2ab680cfde6b9691e68b6342680afaeb39fb0b47
Riccardo Mancinelli <riccardo.mancinelli@cherrybytes.it>
2024-11-20 17:20:22 +0100

    - reviewed structure of login.php and little change in user class data checks

--- login.php
+++ login.php
@@ -26,6 +26,8 @@
         $err_msg_date = null;
         $err_msg_complete = null;
 
+        $error_message = null;
+
         $name = '';
         $surname = '';
         $username = '';
@@ -182,42 +184,90 @@
                 <form action="" method="POST" class='container' novalidate>
                     <h1>Registration</h1>
                     <label for="name">Name * :</label>
-                    <input type="text" name="name" id="name" value="<?php echo $_POST['name'] ??  ''; ?>" required placeholder="insert your name">
+                    <input type="text" name="name" id="name" value="<?= $name; ?>" required placeholder="insert your name" <?= ($err_msg_name) ? "class='input-error'" : ""?>>
+                    <?php
+                        if($err_msg_name){
+                            echo "<p class='errors'>$err_msg_name</p>";
+                        }
+                    ?>
                     <label for="surname">Surname * :</label>
-                    <input type="text" name="surname" id="surname" value="<?php echo $_POST['surname'] ??  ''; ?>" required placeholder="insert your surname">
+                    <input type="text" name="surname" id="surname" value="<?= $surname; ?>" required placeholder="insert your surname" <?= ($err_msg_surname) ? "class='input-error'" : ""?>>
+                    <?php
+                        if($err_msg_surname){
+                            echo "<p class='errors'>$err_msg_surname</p>";
+                        }
+                    ?>
                     <label for="username">Username * : </label>
-                    <input type="text" name="username" id="username" value="<?php echo $_POST['username'] ??  ''; ?>" required placeholder="insert your username">
+                    <input type="text" name="username" id="username" value="<?= $username; ?>" required placeholder="insert your username" <?= ($err_msg_username) ? "class='input-error'" : ""?>>
+                    <?php
+                        if($err_msg_username){
+                            echo "<p class='errors'>$err_msg_username</p>";
+                        }
+                    ?>
                     <label for="email">Email * :</label>
-                    <input type="email" name="email" id="email" value="<?php echo $_POST['email'] ??  ''; ?>" required placeholder="insert your email">
+                    <input type="email" name="email" id="email" value="<?= $email; ?>" required placeholder="insert your email" <?= ($err_msg_email && ($email != '')) ? "class='input-error'" : ""?>>
+                    <?php
+                        if($err_msg_email && ($email != '')){
+                            echo "<p class='errors'>$err_msg_email</p>";
+                        }
+                    ?>
+                    <!-- the password must be retyped each time one error occur -->
                     <label for="password">Password * :</label>
-                    <input type="password" name="password" id="password" required placeholder="insert your password">
+                    <input type="password" name="password" id="password" required placeholder="insert your password" <?= ($err_msg_password && ($password != '')) ? "class='input-error'" : ""?>>
+                    <?php
+                        if($err_msg_password && ($password != '')){
+                            echo "<p class='errors'>$err_msg_password</p>";
+                        }
+                    ?>
                     <label for="confirm_password">Confirm Password * :</label>
-                    <input type="password" name="confirm_password" id="confirm_password" required placeholder="confirm your password">
+                    <input type="password" name="confirm_password" id="confirm_password" required placeholder="confirm your password" <?= ($err_msg_confirm_password) ? "class='input-error'" : ""?>>
+                    <?php
+                        if($err_msg_confirm_password){
+                            echo "<p class='errors'>$err_msg_confirm_password</p>";
+                        }
+                    ?>
                     <label for="date">Birth date : </label>
                     <div>
                         <select name="day" id="date_day">
                             <option value="0">Day</option>
                             <?php for($i = 1; $i <= 31; $i++) : ?>
-                                <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+                                <?php if($i == $day): ?>
+                                    <option value="<?php echo $i; ?>" selected><?php echo $i; ?></option>
+                                <?php else: ?>
+                                    <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+                                <?php endif; ?>
                             <?php endfor; ?>
                         </select>
                         <select name="month" id="date_month">
                             <option value="0">Month</option>
                             <?php for($i = 1; $i <= 12; $i++) : ?>
-                                <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+                                <?php if($i == $month): ?>
+                                    <option value="<?php echo $i; ?>" selected><?php echo $i; ?></option>
+                                <?php else: ?>
+                                    <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+                                <?php endif; ?>
                             <?php endfor; ?>
                         </select>
                         <select name="year" id="date_year">
                             <option value="0">Year</option>
                             <?php for($i = date('Y'); $i >= 1900; $i--) : ?>
-                                <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+                                <?php if($i == $year): ?>
+                                    <option value="<?php echo $i; ?>" selected><?php echo $i; ?></option>
+                                <?php else: ?>
+                                    <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+                                <?php endif; ?>
                             <?php endfor; ?>
                         </select>
                     </div>
+                    <?php
+                        if($err_msg_date && ($day != '' && $month != '' && $year != '')){
+                            echo "<p class='errors'>$err_msg_date</p>";
+                        }
+                    ?>
                     <input type="submit" value="Sign Up">
                     <?php
-                        if($error_message){
-                            echo "<p class='errors'>$error_message</p>";
+                        if($err_msg_complete && (!empty($_POST))){
+                            echo "<p class='errors'>$err_msg_complete</p>";
                         }
                     ?>
                     <p>Already have an account?</p>
